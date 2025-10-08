dda591c6dae13557d054994c9ff0d3f593875c85
DrakoTriz <trizaventuras@gmail.com>
2025-10-08 18:44:36 +0200

    se añade para el usuario la ciudad para que no de error

--- src/routes/usuraio.routes.js
+++ src/routes/usuraio.routes.js
@@ -1,34 +1,48 @@
-const express=require('express');
-const {PrismaClient}=require('@prisma/client');
+const express = require('express');
+const { PrismaClient } = require('@prisma/client');
 
-const prisma=new PrismaClient();
-const router=express.Router();
+const prisma = new PrismaClient();
+const router = express.Router();
 
-//CREAR USUARIO
-router.post('/', async(req , res)=>{
-    try{
-        const {nombre,apellidos,direccion, email, telefono}=req.body;
+// CREAR USUARIO
+router.post('/', async (req, res) => {
+  try {
+    const { nombre, apellidos, direccion, email, telefono, ciudadId } = req.body;
 
-        const nuevoUsuario= await prisma.usuario.create({
-            data: {nombre,apellidos,direccion,email,telefono}
-        });
+    const nuevoUsuario = await prisma.usuario.create({
+      data: {
+        nombre,
+        apellidos,
+        direccion,
+        email,
+        telefono,
+        ciudadId: ciudadId ? Number(ciudadId) : null
+      },
+      include: {
+        ciudad: true
+      }
+    });
 
-        res.json(nuevoUsuario);
-    }catch(error){
-        console.error(error);
-        res.status(500).json({error:"Error creando usuario"});
-    }
+    res.json(nuevoUsuario);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error creando usuario' });
+  }
 });
 
-//LISTAR USUARIOS
-router.get('/', async(req, res)=>{
-    try{
-        const usuarios= await prisma.usuario.findMany();
-        res.json(usuarios);
-    }catch(error){
-        console.error(error);
-        res.status(500).json({error: "Error al obtener ususarios"})
-    }
+// LISTAR USUARIOS
+router.get('/', async (req, res) => {
+  try {
+    const usuarios = await prisma.usuario.findMany({
+      include: {
+        ciudad: true
+      }
+    });
+    res.json(usuarios);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error al obtener usuarios' });
+  }
 });
 
-module.exports=router;
+module.exports = router;
